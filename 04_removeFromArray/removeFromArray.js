df60d1faa71e529be724adbc47f6e408182e0a08
Michelle Xie <93167396+mxie97@users.noreply.github.com>
2023-02-28 17:32:44 -0800

    solve for single argument

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, arg1) {
+    const newArray = array.filter(function (arg) { {
+            return arg1 !== arg;
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
