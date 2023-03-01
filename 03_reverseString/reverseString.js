2f092a3387b23359858284527062ca75adbeca10
Michelle Xie <93167396+mxie97@users.noreply.github.com>
2023-02-28 16:50:37 -0800

    solve reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    myArray = [];
+    myString = '';
+    for (let letter of string) {
+        myArray.push(letter);
+    }
+    for (let i = string.length-1; i >= 0; i--) {
+        myString += myArray[i];
+    }
+    return myString;
 };
 
 // Do not edit below this line
