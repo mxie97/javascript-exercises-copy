a34aaaf321db4b705b903e699576101dcff2be02
Michelle Xie <93167396+mxie97@users.noreply.github.com>
2023-03-02 16:17:48 -0800

    solve sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if (int1 > 0 && int2 > 0 && parseInt(int1)===int1 && parseInt(int2)===int2) {
+        if (int1 > int2) {
+            let int3 = int1;
+            int1 = int2;
+            int2 = int3;
+        }
+        let result = 0;
+        let currentInteger = int1;
+        while (currentInteger <= int2) {
+            result += currentInteger;
+            currentInteger++;
+        };
+        return result; 
+    } else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
