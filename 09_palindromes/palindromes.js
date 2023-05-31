23b7b1f443efb1220858d031b612d02f110c8712
Michelle Xie <93167396+mxie97@users.noreply.github.com>
2023-05-30 17:35:13 -0700

    solve palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
 
+const palindromes = function (str) {
+    let array = str.split("");
+    array = array.filter(item => item.toLowerCase() != item.toUpperCase()).map(item => item.toLowerCase())
+    let isPalindrome = true;
+    if (array.length % 2 === 0) {
+        for (let i=1; i<(array.length/2); i++) {
+            if (array[i-1] != array[array.length-i]) {
+                isPalindrome = false;
+            }
+        }
+    } else {
+        for (let i=1; i<((array.length-1)/2); i++) {
+            if (array[i-1] != array[array.length-i]) {
+                isPalindrome = false;
+            }
+        }
+    }; 
+    return isPalindrome;
 };
 
 // Do not edit below this line
