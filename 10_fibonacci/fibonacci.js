48780e6b8824e0290a4269114bfb5ec67f28a2a5
Michelle Xie <93167396+mxie97@users.noreply.github.com>
2023-05-31 17:11:19 -0700

    solve fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) {
+        return 'OOPS';
+    } 
+    let arr = [1, 1];
+    let result = 1;
+    for (let i=2; i<number; i++) {
+        result = arr[arr.length-1]+arr[arr.length-2];
+        arr.push(result);
+    }
+    return result;
 };
 
 // Do not edit below this line
