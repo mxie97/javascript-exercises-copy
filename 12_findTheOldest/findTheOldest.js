51156c2f9042bd487973c1789124cc0d7edacf26
Michelle Xie <93167396+mxie97@users.noreply.github.com>
2023-05-31 17:36:30 -0700

    solve find the oldeest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    array = array.map(function(item) {
+        if (!item.yearOfDeath) {
+            item.yearOfDeath = new Date().getFullYear() ;
+        } return item;
+    })
+    array.sort( (a, b) => (b.yearOfDeath-b.yearOfBirth)-(a.yearOfDeath-a.yearOfBirth) )
+    return array[0];
 };
 
 // Do not edit below this line
